1b19fd2cfcd60fc9e85664b96969c085b33821cc
rajabzz <saeed.rajabzadeh.95@gmail.com>
2019-05-10 22:00:50 +0430

    Add mongoose and connect to the DB

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const controller = require('./controller');
 const errorHandler = require('./util/errorHandler');
 const jwt = require('./util/jwt');
+const mongodbConfig = require('./configs/mongodb');
 
 const app = express();
 const PORT = 9192;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongodbConfig.uri, {useNewUrlParser: true})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
